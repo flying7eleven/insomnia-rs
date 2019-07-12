b1843789af83fa1f192a2622bde5a2f86b42bf0f
Tim Hütz <tim@huetz.biz>
2019-07-12 13:39:48 +0200

    chore: add code to wait for the next full minute

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use chrono::{Local, Timelike};
+use log::{info, LevelFilter};
+use std::thread::sleep;
+use std::time::Duration;
+
+fn wait_until_full_minute() {
+    let last_timestamp = Local::now().naive_local();
+    sleep(Duration::from_secs(u64::from(60 - last_timestamp.second())));
+}
+
+fn initialize_logging() {
+    // configure the logging framework and set the corresponding log level
+    let logging_framework = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .apply();
+
+    // ensure the logging framework was successfully initialized
+    if logging_framework.is_err() {
+        panic!("Could not initialize the logging framework. Terminating!");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    initialize_logging();
+
+    // wait until we reached the next full minute
+    info!(
+        "The current time is {}. We are waiting for the next full minute to start.",
+        Local::now().naive_local()
+    );
+    wait_until_full_minute();
+
+    // start the recording stuff in minute
 }
