14ff566c7091bf045e11013e27c0f1e67f8de303
Tim Hütz <tim@huetz.biz>
2019-07-26 13:52:03 +0200

    refactor: move the get_available_cards method to the library

--- projects/insomnia/src/lib.rs
+++ projects/insomnia/src/lib.rs
@@ -1,5 +1,14 @@
 use core::fmt;
+use lazy_static::lazy_static;
+use log::{debug, error};
+use regex::bytes::Regex;
+use std::collections::HashMap;
 use std::error;
+use std::process::Command;
+
+lazy_static! {
+    static ref CARD_AND_DEVICES_REGEX: Regex = Regex::new(r"card (\d*):.*device (\d*):").unwrap();
+}
 
 #[derive(Debug, Clone)]
 pub struct AudioDeviceError;
@@ -14,4 +23,56 @@ impl error::Error for AudioDeviceError {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         None
     }
+}
+
+/// Get a list of valid audio cards and their devices.
+///
+/// # Errors
+/// TODO
+///
+/// # Example
+///
+/// Simple way of using this method:
+///
+/// ```
+/// use insomnia::get_available_cards;;
+///
+/// let devices = get_available_cards();
+///
+/// if devices.is_err() {
+///   panic!("Could not get the available audio devices");
+/// }
+///
+/// for device in devices.unwrap() {
+///   println!("Found audio device: {:?}", device);
+/// }
+/// ```
+pub fn get_available_cards() -> Result<HashMap<u8, (u8, u8)>, AudioDeviceError> {
+    let maybe_list_devices_output = Command::new("arecord").args(&["-l"]).output();
+
+    //
+    if maybe_list_devices_output.is_err() {
+        error!("Could not get list of audio devices!");
+        return Err(AudioDeviceError);
+    }
+
+    //
+    let list_devices_output = maybe_list_devices_output.unwrap();
+    let actual_text_output = String::from_utf8_lossy(&list_devices_output.stdout).to_string();
+    let mut device_list = HashMap::new();
+
+    //
+    for cap in CARD_AND_DEVICES_REGEX.captures_iter(actual_text_output.as_bytes()) {
+        let card_id: u8 = String::from_utf8_lossy(&cap[1]).parse().unwrap();
+        let device_id: u8 = String::from_utf8_lossy(&cap[2]).parse().unwrap();
+        debug!("Found audio card {} with device {}", card_id, device_id);
+        device_list.insert(card_id, (card_id, device_id));
+    }
+
+    // if we do not have found any audio devices, also exit with an error
+    if device_list.is_empty() {
+        return Err(AudioDeviceError);
+    }
+
+    Ok(device_list)
 }
--- projects/schlaflosigkeit/src/main.rs
+++ projects/schlaflosigkeit/src/main.rs
@@ -1,18 +1,12 @@
 use chrono::{Local, Timelike};
 use clap::{crate_authors, crate_description, crate_name, crate_version, load_yaml, App};
-use lazy_static::lazy_static;
+use insomnia::get_available_cards;
 use log::{debug, error, info, LevelFilter};
-use regex::bytes::Regex;
 use std::collections::HashMap;
 use std::process::{Command, Stdio};
+use std::thread;
 use std::thread::sleep;
 use std::time::Duration;
-use std::thread;
-use insomnia::AudioDeviceError;
-
-lazy_static! {
-    static ref CARD_AND_DEVICES_REGEX: Regex = Regex::new(r"card (\d*):.*device (\d*):").unwrap();
-}
 
 fn wait_until_full_minute() {
     let last_timestamp = Local::now().naive_local();
@@ -88,56 +82,6 @@ fn convert_audio_file(file_prefix: String) {
             .stdout(Stdio::null())
             .spawn();
     }
-}
-
-/// Get a list of valid audio cards and their devices.
-///
-/// # Errors
-/// TODO
-///
-/// # Example
-///
-/// Simple way of using this method:
-///
-/// ```
-/// let devices = get_available_cards();
-///
-/// if devices.is_err() {
-///   panic!("Could not get the available audio devices");
-/// }
-///
-/// for device in devices.unwrap() {
-///   ...
-/// }
-/// ```
-fn get_available_cards() -> Result<HashMap<u8, (u8, u8)>, AudioDeviceError> {
-    let maybe_list_devices_output = Command::new("arecord").args(&["-l"]).output();
-
-    //
-    if maybe_list_devices_output.is_err() {
-        error!("Could not get list of audio devices!");
-        return Err(AudioDeviceError);
-    }
-
-    //
-    let list_devices_output = maybe_list_devices_output.unwrap();
-    let actual_text_output = String::from_utf8_lossy(&list_devices_output.stdout).to_string();
-    let mut device_list = HashMap::new();
-
-    //
-    for cap in CARD_AND_DEVICES_REGEX.captures_iter(actual_text_output.as_bytes()) {
-        let card_id: u8 = String::from_utf8_lossy(&cap[1]).parse().unwrap();
-        let device_id: u8 = String::from_utf8_lossy(&cap[2]).parse().unwrap();
-        debug!("Found audio card {} with device {}", card_id, device_id);
-        device_list.insert(card_id, (card_id, device_id));
-    }
-
-    // if we do not have found any audio devices, also exit with an error
-    if device_list.is_empty() {
-        return Err(AudioDeviceError);
-    }
-
-    Ok(device_list)
 }
 
 fn is_recording_tool_available() -> bool {
